c32be78c73d37a3b24daffaebaa282d6eb6316a6
Flo <flo.liang@my.com>
2017-12-21 22:46:46 +0000

    Add select airline options in App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,34 @@ import DATA from './data';
 import Table from './components/Table';
 
 class App extends Component {
-  formatValue(property, value) {
+  defaultState = {
+    airline: "all",
+  }
+  
+  constructor(props) {
+    super(props);
+    this.state = this.defaultState;
+  }
+  
+  formatValue = (property, value) => {
     if (property === "airline") {
       return DATA.getAirlineById(value).name;
     } else {
       return DATA.getAirportByCode(value).name;
     }
+  }
+  
+  airlineSelected = (value) => {
+    if (value !== "all") {
+      value = parseInt(value, 10);
+    }
+    
+    this.setState({airline: value});
+  }
+  
+  handleChange = (event) => {
+    event.preventDefault();
+    this.airlineSelected(event.target.value);
   }
   
   render() {
@@ -21,7 +43,22 @@ class App extends Component {
       {name: 'Destination Airport', property: 'dest'},
     ];
     
-    const rows = DATA.routes;
+    const filteredRoutes = DATA.routes.filter((route) => {
+      return this.state.airline === "all" || route.airline === this.state.airline;
+    });
+    
+    const filteredAirlines = DATA.airlines.filter((airline) => {
+      const active = filteredRoutes.some((route) => route.airline === airline.id );
+      return Object.assign({}, airline, {active});
+    });
+    
+    let options = filteredAirlines.map((option) => {
+      const value = option["id"];
+      return <option key={value} value={value}>
+        { option["name"] }
+      </option>;
+    });
+    options.unshift(<option key="all" value="all">All Airlines</option>);
     
     return (
       <div className="app">
@@ -29,10 +66,18 @@ class App extends Component {
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
+          <p>
+            Show routes on
+            <select value={this.state.value} onChange={this.handleChange}>
+              { options }
+            </select>
+            flying
+          </p>
+            
           <Table 
             className="routes-table" 
             columns={columns} 
-            rows={rows} 
+            rows={filteredRoutes} 
             format={this.formatValue}
             perPage={25}
           />
